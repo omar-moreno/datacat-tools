2fdb453d742520d14150761436b9f85df1a0085f
Omar Moreno <email@omarmoreno.net>
2026-01-05 13:27:03 -0800

    Switch to using the click library.

--- crawler/__main__.py
+++ crawler/__main__.py
@@ -1,12 +1,12 @@
-import argparse
 import logging
 import sys
 import time
+
+import click
 import tomli
+from CDMSDataCatalog import CDMSDataCatalog
 
 from crawler import Crawler
-from CDMSDataCatalog import CDMSDataCatalog
-from datetime import date
 
 logging.basicConfig(
     stream=sys.stdout,
@@ -15,57 +15,58 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-# Parse the command line arguments
-parser = argparse.ArgumentParser(
-    description="Data catalog crawler used by the SCDMS experiment."
-)
-parser.add_argument(
+
+@click.command()
+@click.option(
     "-c",
-    action="store",
-    dest="config",
+    "--config",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False),
     help="Path to TOML based configuration file.",
 )
-args = parser.parse_args()
+def main(config):
+    """Data catalog crawler used by the SCDMS experiment."""
 
-if not args.config:
-    parser.error("[ crawler ] A config file needs to be specified.")
+    # Load config from TOML file
+    with open(config, "rb") as f:
+        config = tomli.load(f)
 
-config = None
-with open(args.config, "rb") as f:
-    config = tomli.load(f)
+    dc_config = None
+    if "config" in config["catalog"]:
+        dc_config = config["catalog"]["config"]
+    logging.debug("Configuring the DC client from %s", dc_config)
 
-dc_config = None
-if "config" in config["catalog"]:
-    dc_config = config["catalog"]["config"]
-logging.debug("Configuring the DC client from %s", dc_config)
+    excluded_paths = []
+    if "exclude" in config["crawler"]:
+        excluded_paths = config["crawler"]["exclude"]
+    logging.debug("Excluded paths %s", excluded_paths)
 
-excluded_paths = []
-if "exclude" in config["crawler"]:
-    excluded_paths = config["crawler"]["exclude"]
-logging.debug("Excluded paths %s", excluded_paths)
+    dc = CDMSDataCatalog(config_file=dc_config)
+    crawler = Crawler(dc, config)
 
-dc = CDMSDataCatalog(config_file=dc_config)
-crawler = Crawler(dc, config)
+    paths = None
+    while not paths:
+        paths = dc.ls()
+        if not paths:
+            time.sleep(10)
 
-paths = None
-while not paths:
-    paths = dc.ls()
-    if not paths:
-        time.sleep(10)
+    logging.debug("Paths: %s", paths)
+    paths = list(set(paths).difference(excluded_paths))
 
-logging.debug("Paths: %s", paths)
-paths = list(set(paths).difference(excluded_paths))
+    while True:
+        for path in paths:
+            cpaths = dc.ls(path)
+            if not cpaths:
+                logging.error("Skipping: %s", cpaths)
+                continue
+            for cpath in cpaths:
+                try:
+                    logging.debug("Processing %s", cpath)
+                    crawler.crawl(cpath)
+                except Exception:
+                    logging.error("Skipping path after exception %s", cpath)
+        # logging.info('Missing datasets: %s', len(self.missing_files))
 
-while True:
-    for path in paths:
-        cpaths = dc.ls(path)
-        if not cpaths:
-            logging.error("Skipping: %s", cpaths)
-            continue
-        for cpath in cpaths:
-            try:
-                logging.debug("Processing %s", cpath)
-                crawler.crawl(cpath)
-            except Exception:
-                logging.error("Skipping path after exception %s", cpath)
-    logging.info('Missing datasets: %s', len(self.missing_files))
+
+if __name__ == "__main__":
+    main()
